ee44910d2d8dc083bd90ff076e7d9175928b283d
Musa Dango <musaddangoWgmail.com>
2023-09-29 08:46:14 +0100

    update - removed unwanted package-lock.json

--- server/server.js
+++ server/server.js
@@ -3,20 +3,29 @@ import cors from 'cors';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import knex from 'knex';
+import cookieParser from "cookie-parser";
 
 
-
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : 'localhost',
+      port : 5432,
+      user : 'postgres',
+      password : 'Musa12***',
+      database : 'admin_dashboard_db'
+    }
+  });
 const app = express();
 app.use(cors());
-app.use(express());
-app.use(jwt());
-
+app.use(express.json());
+app.use(cookieParser());
 
 
 app.post('/login', (req, res)=>{
     console.log(req.body);
 })
-const port = 3000;
+const port = 4000;
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`)
 })
